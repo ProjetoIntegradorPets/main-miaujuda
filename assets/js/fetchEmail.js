b469565b56df8e67773769e34ada55e24668851b
MiauJuda <166138829+ProjetoIntegradorPets@users.noreply.github.com>
2025-01-23 10:10:36 -0300

    Delete assets directory

--- assets/js/fetchEmail.js
+++
@@ -1,9 +0,0 @@
-export function fetchEmail(){
-    return fetch('assets/php/validarToken.php')
-    .then(response => {
-        if (!response.ok) {
-            throw new Error('Deu b.o na conexão');
-        }
-        return response.text();
-    });
-}
